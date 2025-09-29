f2c72719e5fc4358b7d58893b3d1b1fa52c364d1
Kinth Enrique Cabuello <blood4249@gmail.com>
2025-09-29 15:33:16 +0800

    made function getHumanChoice

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
-     randomNum = Math.trunc(Math.random() * 3 ) + 1; //Math.trunc() to not make math.random() have decimal 
+    randomNum = Math.trunc(Math.random() * 3 ) + 1; //Math.trunc() to not make math.random() have decimal
     if (randomNum === 1){ 
         return 'rock'
     }else if(randomNum === 2){ 
         return 'paper'
-    }else {
+    }else{
         return 'scissors' 
     }
 }
-
-console.log(getComputerChoice());
+function getHumanChoice(){
+    //.toLowerCase() so that even player inputs uppercase string it will converted to lowercase
+    const choice = prompt('Pck: Rock, Paper, Scissors :').toLowerCase();
+    return choice
+}
